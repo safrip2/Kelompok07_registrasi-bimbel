bc98213c34b3600b1f1c4f52afa8ea7da61ec7c2
Deadila Defiatri <deadiladefiatri04@gmail.com>
2021-05-25 17:27:05 +0700

    soal 4 - 6

--- soal.py
+++ soal.py
@@ -21,8 +21,33 @@ c. 7 - 4√3
 d. -7 - 4√3
 e. -7 + 2√3"""
 
+q4 = """4. Untuk mengerjakan 1 unit rumah dibutuhkan waktu 36 hari dengan 12 tenaga kerja. Berapa waktu akan dihabiskan bila menggunakan 24 orang tenaga kerja?
+a. 14 Hari 
+b. 15 Hari 
+c. 16 Hari 
+d. 17 Hari 
+e. 18 Hari"""
 
-questions = {q1: "c", q2: "", q3: "", q4: "", q6: "", q7: "", q8: "", q9: "", q10: ""}
+q5 = """5. 40% dari a sama dengan b. JIka b adalah 16,67% dari 48, maka a +b =
+a. 8
+b. 14
+c. 24
+d. 28
+e. 36"""
+
+q6 = """"6. Jika p = 4 √12 dan q = 3 √54 maka p + q = …
+a. 8 √3 + 9 √6 
+b. 8 √6 + 9 √3 
+c. 9 √3 + 8 √6 
+d. 3 √8 + 9 √6 
+e. 6 √3 + 9 √8"""
+
+
+
+
+
+
+questions = {q1: "c", q2: "c", q3: "", q4: "e", q5: "d", q6: "a", q7: "", q8: "", q9: "", q10: ""}
 score = 0
 for soal in questions:
     print(soal)
